31bbe47e75749a65ed317540f31290b1daffa1ea
AwfulDarkness0110 <markgarner337@gmail.com>
2023-06-25 09:40:59 +1000

    feat(router): add router starter routes

--- app/src/main.py
+++ app/src/main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# from fastapi_secure_jwt.router.auth import auth_router
+
+app = FastAPI()
+# app.include_router(
+#     auth_router,
+#     prefix="/auth",
+#     tags=["auth"],
+# )
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
---
+++ app/src/schemas/auth.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from pydantic import BaseModel, EmailStr
+
+
+class AuthPayload(BaseModel):
+    fingerprint: str
+
+
+class LoginPayload(AuthPayload):
+    email_or_username: Union[EmailStr, str]
+    password: str
+
+
+class RegisterPayload(AuthPayload):
+    username: str
+    first_name: str
+    last_name: str
+    email: EmailStr
+    password: str
+
+
+class RefreshPayload(BaseModel):
+    refresh_token: str
