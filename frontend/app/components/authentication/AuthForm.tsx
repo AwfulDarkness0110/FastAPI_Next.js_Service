56b5d73a7809ca3a0da2646667b47c11a64b4a00
AwfulDarkness0110 <markgarner337@gmail.com>
2023-06-29 06:21:02 +1000

    feat: request part added

--- frontend/app/components/authentication/AuthForm.tsx
+++ frontend/app/components/authentication/AuthForm.tsx
@@ -1,20 +1,74 @@
 'use client'
 import React, { useState } from "react";
+import { API_SERVER } from "@/app/utils/config";
+import axios from 'axios';
+
 import './style.css'
 
 const Authentication = () => {
   let [authMode, setAuthMode] = useState("signin");
 
-
+  const [username, setUsername] = useState('')
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [loginPassword, setLoginPassword] = useState('')
+  const [usernameOrEmail, setUsernameOrEmail] = useState('')
+  const [registerPassword, setRegisterPassword] = useState('')
+  const [email, setEmail] = useState('')
 
   const changeAuthMode = () => {
     setAuthMode(authMode === "signin" ? "signup" : "signin");
   };
 
+  // Login Action
+  const handleSubmit_Login = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(API_SERVER, {
+        username_or_email: usernameOrEmail,
+        password: loginPassword
+      })
+        .then(async (data) => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  // Register Action
+  const handleSubmit_Register = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(API_SERVER, {
+        username: username,
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        password: registerPassword
+      })
+        .then(async (data) => {
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
   if (authMode === "signin") {
     return (
       <div className="Auth-form-container">
-        <form className="Auth-form">
+        <form className="Auth-form" onSubmit={handleSubmit_Login}>
           <div className="Auth-form-content">
             <h3 className="Auth-form-title">Sign In</h3>
             <div className="text-center">
@@ -29,6 +83,7 @@ const Authentication = () => {
                 type="email"
                 className="form-control mt-1"
                 placeholder="Enter username or email address"
+                onChange={(e) => setUsernameOrEmail(e.target.value)}
               />
             </div>
             <div className="form-group mt-3">
@@ -37,6 +92,7 @@ const Authentication = () => {
                 type="password"
                 className="form-control mt-1"
                 placeholder="Enter password"
+                onChange={(e) => setLoginPassword(e.target.value)}
               />
             </div>
             <div className="d-grid gap-2 mt-3">
@@ -55,7 +111,7 @@ const Authentication = () => {
 
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={handleSubmit_Register}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="text-center">
@@ -69,6 +125,7 @@ const Authentication = () => {
             <input
               className="form-control mt-1"
               placeholder="e.g darkness"
+              onChange={(e) => setUsername(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -76,6 +133,7 @@ const Authentication = () => {
             <input
               className="form-control mt-1"
               placeholder="e.g David"
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -83,6 +141,7 @@ const Authentication = () => {
             <input
               className="form-control mt-1"
               placeholder="e.g Jin"
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -91,6 +150,7 @@ const Authentication = () => {
               type="email"
               className="form-control mt-1"
               placeholder="Email Address"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="form-group mt-3">
@@ -99,6 +159,7 @@ const Authentication = () => {
               type="password"
               className="form-control mt-1"
               placeholder="Password"
+              onChange={(e) => setRegisterPassword(e.target.value)}
             />
           </div>
           <div className="d-grid gap-2 mt-3">
