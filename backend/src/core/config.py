f5c26cffd5c53be80ee6e7e8d3c4486d577d2e88
AwfulDarkness0110 <markgarner337@gmail.com>
2023-07-01 17:41:50 +1000

    fix: alembic setting

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -45,7 +45,7 @@ class DevelopmentSettings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
 
-    PROJECT_NAME: str
+    PROJECT_NAME: str = "AuthencationSystem"
     PROJECT_VERSION: str = "1.0.0"
 
     # CORS
